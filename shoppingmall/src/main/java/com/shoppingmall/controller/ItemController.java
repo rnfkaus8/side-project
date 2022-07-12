3e022e9373a9f2a614d6d5f3aabd17887bc35d90
rnfkaus8 <kth4350@gmail.com>
2022-07-12 18:03:00 +0900

    modify : save 메소드 ItemSave 파라미터에 관하여 검증 기능 추가

--- shoppingmall/src/main/java/com/shoppingmall/controller/ItemController.java
+++ shoppingmall/src/main/java/com/shoppingmall/controller/ItemController.java
@@ -1,8 +1,15 @@
 package com.shoppingmall.controller;
 
+import com.shoppingmall.request.ItemSave;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -11,5 +18,13 @@ public class ItemController {
     @GetMapping("/items")
     public String get() {
         return "hello world";
+    }
+
+    @PostMapping("/items")
+    public Map<String, String> save(@RequestBody @Valid ItemSave request) {
+        log.info("request={}", request);
+        log.info("name={}, price={}", request.getName(), request.getPrice());
+
+        return Map.of();
     }
 }
--- shoppingmall/src/test/java/com/shoppingmall/controller/ItemControllerTest.java
+++ shoppingmall/src/test/java/com/shoppingmall/controller/ItemControllerTest.java
@@ -1,17 +1,17 @@
 package com.shoppingmall.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shoppingmall.request.ItemSave;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.MediaType.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -19,7 +19,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ItemControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     @DisplayName("/items GET 요청 시 Hello World 출력")
@@ -28,7 +31,65 @@ class ItemControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello world"))
                 .andDo(print());
-
     }
 
+    @Test
+    @DisplayName("/items POST 요청 테스트")
+    void save() throws Exception {
+        String content = objectMapper.writeValueAsString(new ItemSave("itemName", 10000));
+        mockMvc.perform(post("/items")
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isOk())
+//                .andExpect()
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("/items POST 요청 시 name 필수, price 필수, price >= 1000 성공")
+    void itemSaveValid() throws Exception {
+        String content = objectMapper.writeValueAsString(new ItemSave("name", 1000));
+        mockMvc.perform(post("/items")
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("/items POST 요청 시 name 필수, price 필수, price >= 1000 실패")
+    void itemSaveValidFailed() throws Exception {
+        mockMvc.perform(post("/items")
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new ItemSave("", 1000))))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+                .andExpect(jsonPath("$.validation.name").value("상품명을 입력하세요."))
+                .andDo(print());
+
+        mockMvc.perform(post("/items")
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new ItemSave("not null", 999))))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+                .andExpect(jsonPath("$.validation.price").value("상품 가격은 1,000 원 이상이어야 합니다."))
+                .andDo(print());
+
+        mockMvc.perform(post("/items")
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new ItemSave(null, null))))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+                .andExpect(jsonPath("$.validation.name").value("상품명을 입력하세요."))
+                .andExpect(jsonPath("$.validation.price").value("상품 가격을 입력하세요."))
+                .andDo(print());
+    }
 }
