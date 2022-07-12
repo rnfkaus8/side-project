31dd591d7ee41fb1fa81ce90d0d63f3dd1fe60dd
rnfkaus8 <kth4350@gmail.com>
2022-07-12 18:03:25 +0900

    modify : name, price 필드의 검증 조건 추가

---
+++ shoppingmall/src/main/java/com/shoppingmall/request/ItemSave.java
@@ -0,0 +1,21 @@
+package com.shoppingmall.request;
+
+import lombok.*;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ItemSave {
+
+    @NotBlank(message = "상품명을 입력하세요.")
+    private String name;
+
+    @NotNull(message = "상품 가격을 입력하세요.")
+    @Min(value = 1000, message = "상품 가격은 1,000 원 이상이어야 합니다.")
+    private Integer price;
+}
