22a52051de9790f7bb54576b9798cd401fe6606a
rnfkaus8 <kth4350@gmail.com>
2022-07-15 12:31:53 +0900

    modify : Item Entity 를 파라미터로 받아 entity -> dto 를 원활하게 도와주는 생성자 추가

--- shoppingmall/src/main/java/com/shoppingmall/response/ItemResponse.java
+++ shoppingmall/src/main/java/com/shoppingmall/response/ItemResponse.java
@@ -1,5 +1,6 @@
 package com.shoppingmall.response;
 
+import com.shoppingmall.domain.Item;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,6 +11,13 @@ public class ItemResponse {
     private String name;
     private int price;
     private String description;
+
+    public ItemResponse(Item item) {
+        this.id = item.getId();
+        this.name = item.getName();
+        this.price = item.getPrice();
+        this.description = item.getDescription();
+    }
 
     @Builder
     public ItemResponse(Long id, String name, int price, String description) {
