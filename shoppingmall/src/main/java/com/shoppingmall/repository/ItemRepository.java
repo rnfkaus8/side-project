bca54d6318adf4a940262c29ee813423fa1da9d3
rnfkaus8 <kth4350@gmail.com>
2022-07-16 13:37:26 +0900

    modify : 상품 리포지토리에 querydsl 적용을 위한 커스텀 리포지토리 클래스 extends

--- shoppingmall/src/main/java/com/shoppingmall/repository/ItemRepository.java
+++ shoppingmall/src/main/java/com/shoppingmall/repository/ItemRepository.java
@@ -1,8 +1,7 @@
 package com.shoppingmall.repository;
 
 import com.shoppingmall.domain.Item;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositoryCustom {
 }
