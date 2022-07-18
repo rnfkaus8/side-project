c3999b14153f13b32bf1ae71947597059c8e72d2
rnfkaus8 <kth4350@gmail.com>
2022-07-18 17:11:53 +0900

    add : 상품 조회 실패용 예외 클래스 생성

---
+++ shoppingmall/src/main/java/com/shoppingmall/exception/ItemNotFound.java
@@ -0,0 +1,14 @@
+package com.shoppingmall.exception;
+
+public class ItemNotFound extends RuntimeException {
+
+    private static final String MESSAGE = "존재하지 않는 상품입니다.";
+
+    public ItemNotFound() {
+        super(MESSAGE);
+    }
+
+    public ItemNotFound(Throwable cause) {
+        super(MESSAGE, cause);
+    }
+}
