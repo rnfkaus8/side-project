7fcb19af397c7ecc550fa231278869d536529ead
rnfkaus8 <kth4350@gmail.com>
2022-07-19 13:14:49 +0900

    modify : 현 프로젝트에 맞는 커스텀 예외처리 클래스(ShoppingmallException) 관련 ExceptionHandler 추가

--- shoppingmall/src/main/java/com/shoppingmall/controller/ExceptionControllerAdvice.java
+++ shoppingmall/src/main/java/com/shoppingmall/controller/ExceptionControllerAdvice.java
@@ -1,29 +1,45 @@
 package com.shoppingmall.controller;
 
+import com.shoppingmall.exception.ShoppingmallException;
 import com.shoppingmall.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 @Slf4j
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse invalidRequestHandler(BindingResult bindingResult) {
+    public ErrorResponse invalidRequestHandler(MethodArgumentNotValidException e) {
         ErrorResponse response = ErrorResponse.builder()
                 .code("400")
                 .message("잘못된 요청입니다.")
                 .build();
-        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+        for (FieldError fieldError : e.getFieldErrors()) {
             response.addValidation(fieldError.getField(), fieldError.getDefaultMessage());
         }
         return response;
+    }
+
+    @ExceptionHandler(ShoppingmallException.class)
+    public ResponseEntity<ErrorResponse> shppingmallException(ShoppingmallException e) {
+        int statusCode = e.getStatusCode();
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code(String.valueOf(statusCode))
+                .message(e.getMessage())
+                .validation(e.getValidation())
+                .build();
+
+        return ResponseEntity.status(statusCode)
+                .body(errorResponse);
     }
 }
